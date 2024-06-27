901bf543635d614c52a5f5551bf619efc10f1dd3
Mominul islam <72146741+Mominul-Islam-cmd@users.noreply.github.com>
2024-06-27 11:02:22 +0600

    Update settings.py

--- diabetic/settings.py
+++ diabetic/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-5)*5%67#&qugq)0($e22%&lnca@38e+txwms!0$tpu^b@tuag9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
